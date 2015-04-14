d4a45254e31530efeb6d3c6ed41851e2b6709b05
Bastian Fredriksson <KTH-CSC>
2015-04-14 17:26:09 +0200

    Moved Watts-Strogatz to kex.generators package.

--- src/kex/generators/GraphGenerator.java
+++
@@ -1,9 +0,0 @@
-package kex.generators;
-
-public interface GraphGenerator {
-	/**
-	 * Generates a random graph.
-	 * @param The number of vertices in the graph.
-	 */
-	public String generate(int vertexCount);
-}
